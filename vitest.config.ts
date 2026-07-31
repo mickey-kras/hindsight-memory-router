98edbbf24e68eb996d13481e159d028b9d18d46c
Mikhail Krasilnikov <mickey.krasilnikov@gmail.com>
2026-07-31 00:37:23 -0700

    Exclude declarations from coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     coverage: {
       provider: "v8",
       include: ["src/**/*.ts"],
-      exclude: ["src/**/*.test.ts"],
+      exclude: ["src/**/*.test.ts", "src/**/*.d.ts"],
       reporter: ["text", "lcov", "json-summary"],
       reportOnFailure: true,
       thresholds: {
